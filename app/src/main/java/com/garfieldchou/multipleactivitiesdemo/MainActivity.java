1b7431effb27908dcad433c4f898020a7017415a
shaudai <shaudai0402@hotmail.com>
2017-01-13 22:37:27 +0800

    Add a button for jumping to the second activity

--- app/src/main/java/com/garfieldchou/multipleactivitiesdemo/MainActivity.java
+++ app/src/main/java/com/garfieldchou/multipleactivitiesdemo/MainActivity.java
@@ -1,9 +1,19 @@
 package com.garfieldchou.multipleactivitiesdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void toSecondActivity(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
