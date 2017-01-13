414b423ec5518e64e4bb7bc41e63042e287381f9
shaudai <shaudai0402@hotmail.com>
2017-01-13 23:26:08 +0800

    Create a ListView for the names of friends, and show the corresponding toast at the second activity
    ListView
    .setArrayAdapter
    .setOnItemClickListener
    
    ArrayList
    
    ArrayAdapter
    
    AdapterView.onItemClickListener

--- app/src/main/java/com/garfieldchou/multipleactivitiesdemo/MainActivity.java
+++ app/src/main/java/com/garfieldchou/multipleactivitiesdemo/MainActivity.java
@@ -3,22 +3,42 @@ package com.garfieldchou.multipleactivitiesdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
-
-    public void toSecondActivity(View view) {
-
-        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-        intent.putExtra("username", "garfield");
-
-        startActivity(intent);
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView friendsName = (ListView)findViewById(R.id.friendsName);
+
+        final ArrayList<String> listFriends = new ArrayList<String>(asList("CH", "Kevin", "Vic", "Dino"));
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listFriends);
+
+        friendsName.setAdapter(arrayAdapter);
+
+        friendsName.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Log.i("Info", listFriends.get(i));
+
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("friend", listFriends.get(i));
+
+                startActivity(intent);
+            }
+        });
     }
 }
