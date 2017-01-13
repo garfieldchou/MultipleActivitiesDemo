a10697fe6ecd4c1f261b13b5a2a5f91fe1156caa
shaudai <shaudai0402@hotmail.com>
2017-01-13 22:44:16 +0800

    Add a button in the second activity for jumping back to the main activity

--- app/src/main/java/com/garfieldchou/multipleactivitiesdemo/SecondActivity.java
+++ app/src/main/java/com/garfieldchou/multipleactivitiesdemo/SecondActivity.java
@@ -1,9 +1,19 @@
 package com.garfieldchou.multipleactivitiesdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public void backToMain(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
