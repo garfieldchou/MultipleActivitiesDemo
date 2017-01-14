59c6d4364427ccf00650f27cbee4e7d783a05203
shaudai <shaudai0402@hotmail.com>
2017-01-14 23:03:28 +0800

    Set a ListView for four names of friends

--- app/src/main/java/com/garfieldchou/multipleactivitiesdemo/MainActivity.java
+++ app/src/main/java/com/garfieldchou/multipleactivitiesdemo/MainActivity.java
@@ -4,21 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void toSecondActivity(View view) {
-
-        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-        intent.putExtra("username", "garfield");
-
-        startActivity(intent);
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        ArrayList<String> friends = new ArrayList<String>();
+
+        friends.add("Monica");
+        friends.add("Chandler");
+        friends.add("Joey");
+        friends.add("Rachel");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, friends);
+
+        listView.setAdapter(arrayAdapter);
+                
     }
 }
